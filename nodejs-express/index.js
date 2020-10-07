fd861d83acfe49e76103b51754560868ee3bbbbb
thisisminh172 <thisisminh172@gmail.com>
2020-10-07 20:47:16 +0700

    first commit

---
+++ nodejs-express/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', function(request, response) {
+    response.send('<h1>Hello world</h1>');
+});
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
